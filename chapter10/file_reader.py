3e136b14ffd7d317ef67e342ce40c602065ffc5b
kevin <271880517@qq.com>
2018-12-27 10:55:10 +0800

    chapter10

--- chapter10/file_reader.py
+++ chapter10/file_reader.py
@@ -4,3 +4,29 @@
 with open('pi_digits.txt') as file_object:
     contents = file_object.read()
     print(contents)
+
+# 逐行读取
+filename = 'pi_digits.txt'
+with open(filename) as file_object:
+    for line in file_object:
+        print(line.rstrip())
+
+# 包含文件内容的列表
+filename = 'pi_digits.txt'
+with open(filename) as file_object:
+    lines = file_object.readlines()
+
+for line in lines:
+    print(line.rstrip())
+
+# 使用文件的内容
+filename = 'pi_digits.txt'
+with open(filename) as file_object:
+    lines = file_object.readlines()
+
+pi_string = ''
+for line in lines:
+    pi_string += line.strip()
+
+print(pi_string)
+print(len(pi_string))
---
+++ chapter10/test10.1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# 10-1 Python学习笔记
+filename = 'learning_python.txt'
+
+with open(filename) as file_object:
+    print(file_object.read())
+
+with open(filename) as file_object:
+    for line in file_object:
+        print(line.rstrip())
+
+with open(filename) as file_object:
+    lines = file_object.readlines()
+for line in lines:
+    print(line.rstrip())
